cbae2d42260ca4157bac0a735140935444ac4074
pengliang <15616205476@163.com>
2020-08-16 17:12:03 +0800

    学生类

--- app/src/main/java/com/example/gitstudy/MainActivity.java
+++ app/src/main/java/com/example/gitstudy/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate: 新功能开发");
+        Log.d(TAG, "onCreate: xinfenzhi");
 
     }
 }
